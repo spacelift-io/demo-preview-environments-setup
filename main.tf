e97fef3c9164a47339ca161e26923b1259dff9de
Jakub Martin <jakub.wit.martin@gmail.com>
2021-03-23 17:44:50 +0100

    formatting

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ resource "spacelift_stack" "manager" {
   name = "Demo Preview Environments Manager"
 
   administrative = true
-  autodeploy = true
-  branch = "master"
-  repository = var.manager_repository
+  autodeploy     = true
+  branch         = "master"
+  repository     = var.manager_repository
 
   labels = [
     "destroy_on_delete",
@@ -13,7 +13,7 @@ resource "spacelift_stack" "manager" {
 }
 
 resource "spacelift_policy_attachment" "manager-trigger_policy" {
-  stack_id = spacelift_stack.manager.id
+  stack_id  = spacelift_stack.manager.id
   policy_id = spacelift_policy.manager-trigger_policy.id
 }
 
@@ -29,15 +29,15 @@ resource "spacelift_aws_role" "hello-service" {
 }
 
 resource "spacelift_environment_variable" "aws_role" {
-  stack_id = spacelift_stack.manager.id
-  name = "TF_VAR_aws_role"
-  value = var.aws_role
+  stack_id   = spacelift_stack.manager.id
+  name       = "TF_VAR_aws_role"
+  value      = var.aws_role
   write_only = false
 }
 
 resource "spacelift_environment_variable" "domain_name" {
-  stack_id = spacelift_stack.manager.id
-  name = "TF_VAR_domain_name"
-  value = var.domain_name
+  stack_id   = spacelift_stack.manager.id
+  name       = "TF_VAR_domain_name"
+  value      = var.domain_name
   write_only = false
 }
